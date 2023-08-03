1171c3e795898a202d30849a52d40e61f47748e1
verah makori <verahmakori@gmail.com>
2023-08-03 17:20:33 +0300

    added rangers dao

--- src/main/java/org/saf/DAO/Animal_DAO.java
+++ src/main/java/org/saf/DAO/Animal_DAO.java
@@ -14,4 +14,41 @@ public class Animal_DAO {
             db.createQuery(tableSpell).executeUpdate();
         } catch (Exception error) {System.out.println(error.getMessage());}
     }
+
+    //ANIMAL DATA ADDED INTO THE ANIMALS TABLE
+    public static void addAnimals (Animals newAnimal) {
+        try (Connection db = database.getConnection().open()) {
+            String addingSpell = "INSERT INTO animals (animal, endangered) VALUES (UPPER(:animal), (:endangered));";
+            db.createQuery(addingSpell).bind(newAnimal).executeUpdate();
+        } catch (Exception error) { System.out.println(error.getMessage());}
+    }
+
+    //RETURNS ALL ANIMALS
+    public static List<Animals> allAnimals (){
+        List<Animals> animalList = null;
+        try(Connection db = database.getConnection().open()) {
+            String querySpell = "SELECT * FROM animals WHERE NOT deleted; ";
+            animalList = db.createQuery(querySpell).executeAndFetch(Animals.class);
+        } catch (Exception error){ System.out.println(error.getMessage());}
+        return animalList;
+    }
+
+    //GETS A PARTICULAR ANIMAL FROM THE ANIMALS TABLE
+    public static List<Animals> oneAnimal (String animal) {
+        List<Animals> oneAnimalList = null;
+        try(Connection db = database.getConnection().open()){
+            String oneAnimalSpell = "SELECT * FROM animals WHERE animal = (:animal) AND NOT deleted;";
+            oneAnimalList = db.createQuery(oneAnimalSpell).addParameter("animal", animal).executeAndFetch(Animals.class);
+        } catch (Exception error) { System.out.println(error.getMessage());}
+        return oneAnimalList;
+    }
+
+    //DELETES A SQUAD FROM THE DATABASE
+    public static void deleteAnimal(String animal){
+        try(Connection db = database.getConnection().open()){
+            String deletionSpell = " UPDATE animals SET deleted = true WHERE animal = (:animal);";
+            db.createQuery(deletionSpell).addParameter("animal", animal).executeUpdate();
+        } catch (Exception error) { System.out.println(error.getMessage());}
+    }
+
 }
