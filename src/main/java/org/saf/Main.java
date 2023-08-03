0017c60cbd2c7bacec2bf1c814af8ff470c0388c
verah makori <verahmakori@gmail.com>
2023-08-03 17:52:30 +0300

    added hbs file

--- src/main/java/org/saf/Main.java
+++ src/main/java/org/saf/Main.java
@@ -1,7 +1,21 @@
 package org.saf;
+import org.saf.DAO.Animal_DAO;
+import org.saf.DAO.Rangers_DAO;
+import org.saf.DAO.Sighting_DAO;
+import org.saf.models.Animals;
+import org.saf.models.Rangers;
+import org.saf.models.Sighting;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        staticFileLocation("/public");
+        HandlebarsTemplateEngine views = new HandlebarsTemplateEngine();
     }
 }
